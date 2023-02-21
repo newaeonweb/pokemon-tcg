45e16f38ba49d0868c4d87ddf05793f751f7f440
newaeonweb <fsilvasmonteiro@gmail.com>
2023-02-21 20:04:17 -0300

    wip add tailwind and disable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'pokemon-tcg-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
